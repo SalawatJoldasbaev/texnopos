3f94396558e99317021c63edfbe9e175a2b67c4c
Ashirbek0918 <ashirbekrajapboyev@gmail.com>
2022-06-21 17:41:58 +0500

    Imagecontroller

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(ImageController::class)->group(function(){
+    Route::post('image/upload','upload');
+    Route::delete('image/delete/{filename}','delete');
+    Route::get('/images','allImages');
 });
