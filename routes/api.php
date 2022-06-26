2f1f31b616149ed54f4f5a2a267ce110198d5ef9
Ashirbek0918 <ashirbekrajapboyev@gmail.com>
2022-06-26 16:12:06 +0500

    Blogscontroller successfuly craeted

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\TeamsController;
@@ -17,12 +18,19 @@ Route::controller(TeamsController::class)->group(function(){
 });
 Route::controller(PortfolioController::class)->group(function(){
     Route::get('allportfolios','allPortfolios');
-    Route::get('onePortfolio/{portfolio}','onePortfolio');
+    Route::get('oneportfolio/{portfolio}','onePortfolio');
 });
 Route::controller(NewsController::class)->group(function(){
-    Route::get('allnews/{type}','getNews');
+    Route::get('allnews','getNews');
     Route::get('onenews/{news}','oneNews');
 });
+Route::controller(BlogController::class)->group(function(){
+    Route::get('allblogs','getBlogs');
+    Route::get('oneblog/{blog}','oneblog');
+});
+
+Route::get('/teacher/show',[TeacherController::class,'show']);
+Route::get('course/show',[CourseController::class,'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::controller(ImageController::class)->group(function () {
@@ -41,7 +49,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::controller(TeacherController::class)->group(function() {
         Route::post('/teacher/create', 'create');
-        Route::get('/teacher/show', 'show');
         Route::put('/teacher/update/{id}', 'update');
         Route::delete('/teacher/delete/{id}', 'delete');
         Route::get('/teacher/history', 'history');
@@ -49,7 +56,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::controller(CourseController::class)->group(function(){
         Route::post('/course/create', 'create');
-        Route::get('/course/show', 'show');
         Route::put('/course/update/{id}', 'update');
         Route::delete('/course/delete/{id}', 'delete');
         Route::get('/course/history', 'history');
@@ -59,6 +65,11 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('news/create','create');
         Route::delete('news/delete/{news}','delete');
         Route::put('news/update/{news}','update');  
+    });
+    Route::controller(BlogController::class)->group(function(){
+        Route::post('blog/create','create');
+        Route::delete('blog/delete/{blog}','delete');
+        Route::put('blog/update/{blog}','update');  
     });
     Route::controller(PortfolioController::class)->group(function(){
         Route::post('portfolio/create','create');
